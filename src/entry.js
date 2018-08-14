7ab9d2c28ab45a429ff324620fca7319965d9048
SAIRAMMVK <sairamkrishna497@gmail.com>
2018-08-14 19:13:25 +0530

    seperated objects

--- src/entry.js
+++ src/entry.js
@@ -7,14 +7,14 @@ class EntryContent extends Component {
                 <form>
                     <div className="form-group">
                         <label htmlFor="eid">Employee ID:</label>
-                        <input type="text" className="form-control w3-input w3-border w3-hover-blue" 
-                            onChange={this.props.changeValue} id="eid" 
+                        <input type="text" className="form-control w3-input w3-border w3-hover-blue"
+                            onChange={this.props.changeValue} id="eid"
                             placeholder="Enter employee id" name="eid" />
                     </div>
                     <div className="form-group">
-                        <button type="button" id="swipein" onClick={this.props.setEntry}  className="btn btn-primary" >Swipe In</button>
-                        <button type="button" id="swipeout" onClick={this.props.changeEntry} className="btn btn-primary" >Swipe Out</button>                        
-                        
+                        <button type="button" id="swipein" onClick={this.props.setEntry} className="btn btn-primary" >Swipe In</button>
+                        <button type="button" id="swipeout" onClick={this.props.changeEntry} className="btn btn-primary" >Swipe Out</button>
+
                     </div>
                 </form>
             </div>
--- src/mainfile.js
+++ src/mainfile.js
@@ -1,133 +1,156 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import EntryContent from './entry';
 import DisplayContent from './display';
-import { WSAEINTR, DH_UNABLE_TO_CHECK_GENERATOR } from 'constants';
 
-class MainFile extends Component{
+
+class MainFile extends Component {
     constructor() {
-        let fieldvalue, swipeindate,swipeInTimeHours,swipeoutdate, swipeInTimeMinutes, startTime, stayTime;
-        let emparray = [], empentry = [], empexit=[];
+        let fieldvalue, swipeindate, swipeInTimeHours, swipeoutdate, swipeInTimeMinutes, startTime, stayTime;
+        let emparray = [], empentry = [], empexit = [], stayarray = [], stayemp = [];
         super();
         this.state = {
-          entry: {
-            employees: [],
-            entrytimes: []
-          },
-          exit:{
-            employees: [],
-            exittimes: []
-          },
-          stay:{
-              employees:[],
-              staytimes:[]
-          }
+            entry: {
+                employees: [],
+                entrytimes: []
+            },
+            exit: {
+                employees: [],
+                exittimes: []
+            },
+            stay: {
+                employees: [],
+                staytimes: []
+            },
+            emptable:{
+                empid:[],
+                empintime:[],
+                empouttime:[],
+                empstaytime:[]
+            }
         }
         this.updateEntry = this.updateEntry.bind(this);
         this.changeState = this.changeState.bind(this);
         this.changeTime = this.changeTime.bind(this);
-      }
-    
-      changeTime() {
+    }
 
-        this.emparray = this.state.exit.employees;
-        this.emparray.push(this.fieldvalue);
+    changeTime() {
 
-        //calculating the hours of stay in office
-    
-        let outTimeHours = new Date().getHours();
-        let outTimeMinutes = new Date().getMinutes();
-        let endTime = parseInt(outTimeHours.toString() + outTimeMinutes.toString())
-    
-        this.stayTime = ((endTime - this.startTime)/100);
-        //difference time 
-        console.log("You stayed for " + this.stayTime + " hours")
+        if (this.fieldvalue) {
 
-        //storing the time of leaving
-        this.empexit = this.state.exit.exittimes;
-        let outTime = new Date().toLocaleString();
+            this.emparray = this.state.exit.employees;
+            this.emparray.push(this.fieldvalue);
 
-        this.empexit.push(outTime);
+            this.stayemp = this.state.stay.employees;
+            this.stayemp.push(this.fieldvalue);
 
-        this.setState({
-            exit:{
-                exittimes:this.empexit,
-                employees:this.emparray
-            }
-        }) 
-        console.log(this.state.exit) 
-                
-      }
-    
-      updateEntry() {
-        //pushing the employee details to employees array present in state
-        this.emparray = this.state.entry.employees;
-        this.emparray.push(this.fieldvalue);
-    
-        //creating the time entry on click of the button
-        this.swipeindate = new Date();
-        this.swipeintime = this.swipeindate.getHours();
-        this.swipeInTimeMinutes = this.swipeindate.getMinutes();
-        this.startTime = parseInt(this.swipeintime.toString() + this.swipeInTimeMinutes.toString());
-        
-        this.empentry = this.state.entry.entrytimes;
-        this.empentry.push(this.swipeindate.toLocaleString());
-    
-        this.setState({
-          entry:{
-            employees: this.emparray,
-            entrytimes: this.empentry
-          }
-        })
-    
-        //code for enabling the swipeout button
-        // setTimeout(function () {
-        //  document.getElementById("swipeout").removeAttribute("disabled")
-        // }, 60000 * 0.5)
-    
-      }
-    
-      changeState(event) {
-        this.fieldvalue = event.target.value;
-        /*if(this.fieldvalue){
-            document.getElementById("swipein").removeAttribute("disabled");
-        }*/
-      }
-    
-    render(){
-        return(
+            //calculating the hours of stay in office
+
+            let outTimeHours = new Date().getHours();
+            let outTimeMinutes = new Date().getMinutes();
+            let endTime = parseInt(outTimeHours.toString() + outTimeMinutes.toString())
+
+            this.stayTime = ((endTime - this.startTime) / 100);
+            this.stayarray = this.state.stay.staytimes;
+            this.stayarray.push(this.stayTime);
+           
+            //storing the time of leaving
+            this.empexit = this.state.exit.exittimes;
+            let outTime = new Date().toLocaleString();
+
+            this.empexit.push(outTime);
+
+
+            this.setState({
+                exit: {
+                    exittimes: this.empexit,
+                    employees: this.emparray
+                },
+                stay: {
+                    employees: this.stayemp,
+                    staytimes: this.stayarray
+                }
+            })
+
+            console.log(this.state)
+
+        }
+    }
+
+    updateEntry() {
+
+        if (this.fieldvalue) {
+
+            //pushing the employee details to employees array present in state
+            this.emparray = this.state.entry.employees;
+            this.emparray.push(this.fieldvalue);
+
+            //creating the time entry on click of the button
+            this.swipeindate = new Date();
+            this.swipeintime = this.swipeindate.getHours();
+            this.swipeInTimeMinutes = this.swipeindate.getMinutes();
+            this.startTime = parseInt(this.swipeintime.toString() + this.swipeInTimeMinutes.toString());
+
+            this.empentry = this.state.entry.entrytimes;
+            this.empentry.push(this.swipeindate.toLocaleString());
+
+
+            this.setState({
+                entry: {
+                    employees: this.emparray,
+                    entrytimes: this.empentry
+                }
+            })
+
+            //code for enabling the swipeout button
+            // setTimeout(function () {
+            //  document.getElementById("swipeout").removeAttribute("disabled")
+            // }, 60000 * 0.5)
+        }
+    }
+
+    changeState(event) {
+        let val = event.target.value;
+        let pattern = /[a-zA-Z]/g;
+        let result = val.match(pattern);
+        if (result) console.log("match is there")
+        else this.fieldvalue = event.target.value;
+    }
+
+    render() {
+        return (
             <div className="App">
 
-        <div className="App-header">
+                <div className="App-header">
 
-          <header>TIME ENTRY PORTAL</header>
+                    <header>TIME ENTRY PORTAL</header>
 
-        </div>
+                </div>
 
-        <div className="content">
+                <div className="content">
 
-          <h3><b>Welcome My Dear Friend!!</b></h3>
+                    <h3><b>Welcome My Dear Friend!!</b></h3>
 
-          <div className="entrycontent">
+                    <div className="entrycontent">
 
-            <EntryContent changeEntry={this.changeTime} changeValue={this.changeState} entries={this.state.entry.employees} setEntry={this.updateEntry} />
+                        <EntryContent changeEntry={this.changeTime} changeValue={this.changeState} entries={this.state.entry.employees} setEntry={this.updateEntry} />
 
-          </div>
+                    </div>
 
-          <div className="displaycontent">
+                    <div className="displaycontent">
 
-            <DisplayContent empnames={this.state.entry.employees} empentries={this.state.entry.entrytimes} />
+                        <DisplayContent empnames={this.state.entry.employees} empentries={this.state.entry.entrytimes} />
 
-          </div>
+                    </div>
 
-        </div>
+                </div>
 
-        <div className="footer">
+                <div className="footer">
 
-          <footer>&copy; reserved by David Billa, Catch If You Can</footer>
+                    <footer>&copy; reserved by David Billa, Catch If You Can</footer>
 
-        </div>
+                </div>
 
-      </div>
+            </div>
         );
     }
 }
